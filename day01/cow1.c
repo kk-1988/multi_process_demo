a6ec8a10a22ba9efebb99e5c66704605fa879a56
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-10 19:10:56 +0800

    fork写时拷贝demo

---
+++ day01/cow1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int data = 10;
+
+void child_process()
+{
+	printf("Child process %d, data %d\n", getpid(), data);
+	data = 20;	//page fault
+	printf("Child process %d, data %d\n",getpid(), data);
+	_exit(0);
+}
+
+int main(int argc,char *argv[])
+{
+	int pid;
+	
+	pid = fork();
+	
+	if(-1 == pid)
+		exit(-1);
+	
+	if( 0 == pid)
+	{
+		child_process();
+	}
+	else
+	{
+		sleep(1);
+		printf("Parent process %d, data %d\n",getpid(), data);
+	}
+	return 0;
+}
