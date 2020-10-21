1914dfc5f03716b203453ca1945b4dfc2dcbd7ff
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-21 19:18:14 +0800

    vfork 修改

--- day01/vfork.c
+++ day01/vfork.c
@@ -8,13 +8,16 @@ void child_process()
 	printf("Child process %d, data %d\n", getpid(), data);
 	data = 20;	//page fault
 	printf("Child process %d, data %d\n",getpid(), data);
-	_exit(0);
+	//_exit(0);
+	while(1);
 }
 
 int main(int argc,char *argv[])
 {
 	int pid;
 	
+	//1.写时不拷贝
+	//2.阻塞的
 	pid = vfork();
 	
 	if(-1 == pid)
